549ef8a7ae49ac7d052547d22608f8d475cacb37
goosedude <60986278+goosedude@users.noreply.github.com>
2020-06-03 04:46:00 -0700

    Created Dashboard

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -20,7 +20,7 @@ router.get('/me', auth, async (req, res) => {
 
         res.json(profile);
 
-    } catch (error) {
+    } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error')
     }
