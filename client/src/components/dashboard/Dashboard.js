e1aede47a9bbb6589994bc89944d7a071332ff17
goosedude <60986278+goosedude@users.noreply.github.com>
2020-06-03 23:47:43 -0700

    Updated Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,18 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
+import Spinner from '../layout/Spinner';
 
 const Dashboard = ({
     getCurrentProfile,
-    auth,
-    profile
+    auth: { user },
+    profile: { profile, loading }
 }) => {
     useEffect(() => {
         getCurrentProfile();
     }, [getCurrentProfile]);
 
-    return <div> Dashboard Test Demo</div>;
+    return loading && profile === null ? (
+        <Spinner />
+    ) : (
+            <Fragment>
+                <h1 className="large text-primary">Dashboard</h1>
+                <p className="lead">
+                    <i className="fas fa-user"></i>Welcome {user && user.name}</p>
+                {profile !== null ? (
+                    <Fragment>
+
+                    </Fragment>
+                ) : (
+                        <Fragment>
+                            <p>No profile exists! Create a profile. </p>
+                            <Link to='/create-profile' className="btn btn-primary my-1">
+                                Create Profile
+                        </Link>
+                        </Fragment>
+                    )}
+            </Fragment>
+        );
 };
 
 Dashboard.propTypes = {
